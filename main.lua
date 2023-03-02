2a75c683dba6b8f8ec9e358ed02b8cf2a3a1227d
windy <707298413@qq.com>
2023-03-02 20:48:52 +0800

    add example3

--- main.lua
+++ main.lua
@@ -41,8 +41,51 @@ local function example2()
 end
 
 
+local function test(f, name)
+	local c = os.clock()
+	f()
+	print(string.format("Test %s done, use %fs", name, os.clock() - c))
+end
 
-example1()
-example2()
+
+-- 10W 级别的测试 (将数据完全加载至内存前的 IO测试 我的机器大概是 1秒 20万个 对象，存储文件大小4.2MB)
+local function example3( )
+	print('multiple objects example ------------------------------------->')
+
+	local ex3_config = ldb.start{name = "ex3_config"}
+	local objects = ldb.start{name = "objects", multiple = 100000, profile = {id = true, gold = true}}
+
+	local init_tag = "ex3_initialized_v1"
+
+	local max = 100000
+
+	local function init()
+		for i=1,max do
+			objects.set(i, {id = i, gold = i*100, name = "nick"..i})
+		end
+	end
+
+	local function get_all()
+		for i=1,max do
+			objects.get(i)
+		end
+	end
+
+	if not ex3_config.get(init_tag) then
+		test(init, init_tag)
+	else
+		test(get_all, "get all objects")
+	end
+
+	dump(objects.get(1))
+
+	ex3_config.set(init_tag, true)
+end
+
+
+
+-- example1()
+-- example2()
+example3()
 
 print('bye')
